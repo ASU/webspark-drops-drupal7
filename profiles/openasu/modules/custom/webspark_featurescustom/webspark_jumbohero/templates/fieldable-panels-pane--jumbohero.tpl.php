307c19c372d6b7d28512c178f03a082282646806
Bryan <aubjr@asu.edu>
2014-11-20 17:00:25 -0700

    WEBSPARK-379 - Add 700px tall hero image feature; updated openasu.info

---
+++ profiles/openasu/modules/custom/webspark_featurescustom/webspark_jumbohero/templates/fieldable-panels-pane--jumbohero.tpl.php
@@ -0,0 +1,8 @@
+<div class="jumbotext-wrapper position-<?php print render($content['field_webspark_jumbo_position']['#items'][0]['value']); ?> <?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($elements['title']); ?>
+  <?php print render($title_suffix); ?>
+  <?php print $fields; ?>
+  
+
+</div>
