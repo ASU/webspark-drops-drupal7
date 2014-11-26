a5057d00dd005bb76b740622b3dbb0dc51d7a0b0
Bryan <aubjr@asu.edu>
2014-11-26 16:13:56 -0700

    Arizona sprint - Updated Webspark Panels styles - removed pane, region TPLs

--- profiles/openasu/modules/custom/webspark_featurescustom/webspark_panels_styles/plugins/styles/customstyles/customstyles-region.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="customstyles-region work-region-it  <?php print $classes; ?> <?php print (isset($settings['fullwidthbg'])) ? $settings['fullwidthbg'] : 'max-payne'; ?>
-<?php print (isset($settings['panerowpad'])) ? $settings['panerowpad'] : ''; ?>
-<?php print (isset($settings['panespace'])) ? $settings['panespace'] : ''; ?>">
-  <?php print render($content); ?>
-</div>
