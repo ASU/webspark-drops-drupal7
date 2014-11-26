fd1ed4ea3a5438fe227f5877bfc22fa6ffd73287
Bryan <aubjr@asu.edu>
2014-11-26 16:11:19 -0700

    Arizona sprint - Updated Webspark Hero, Webspark Jumbo Hero modules in featurescustom

--- profiles/openasu/modules/custom/webspark_featurescustom/webspark_hero/js/webspark_hero.js
+++ profiles/openasu/modules/custom/webspark_featurescustom/webspark_hero/js/webspark_hero.js
@@ -8,6 +8,7 @@
 		attach: function(context, settings) {
 		
 		  $(".pane-bundle-hero").wrapInner( "<div class='hero-wrapper'></div>");
+		  $(".pane-bundle-hero").addClass( "hero-bg-img");
 		  
 		}
 	};
