7a2ed28b41a62a7ddd0a7d09da8baf0178a488eb
Bryan <aubjr@asu.edu>
2014-12-05 03:54:38 -0700

    Arizona II - Updated Webspark News & Events feature

--- profiles/openasu/modules/custom/webspark_featurescustom/webspark_newsevents/js/webspark_newsevents.js
+++ profiles/openasu/modules/custom/webspark_featurescustom/webspark_newsevents/js/webspark_newsevents.js
@@ -3,15 +3,26 @@
 	Drupal.behaviors.webspark_newsevents = {
 		attach: function (context, settings) {			
 
-      /** 
-       * Hide image if news article doesn't have its image and is using default image.
-       */
+      /* ASU News node */
 			if( $("body").hasClass("node-type-asu-news") ) {
 				$(document).ready(function() {
+          
+          /** 
+           * Hide image if news article doesn't have its image and is using default image.
+           */
           src = $('.field-name-field-asunews-image img').attr('src');
           if(src.indexOf("asunews_default_image.png") > -1) {
             $('.pane-node-field-asunews-image').remove();
           }
+          
+          /** 
+           * Change right side bar to be col-md-4. The rest will be col-md-8.
+           */
+          $("div.bryant-flipped-content-region").removeClass("col-md-9");
+          $("div.bryant-flipped-content-region").addClass("col-md-8");
+          
+          $("div.bryant-flipped-sidebar-region").removeClass("col-md-3");
+          $("div.bryant-flipped-sidebar-region").addClass("col-md-4");
 				});
 			}
 		 
