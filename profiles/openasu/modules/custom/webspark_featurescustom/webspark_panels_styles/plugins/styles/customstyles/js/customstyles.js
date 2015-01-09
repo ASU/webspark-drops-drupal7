7e82c7fce43b857207d3e87d7230f060a4e48b69
Bryan <aubjr@asu.edu>
2015-01-09 16:01:18 -0700

    New Mexico - Updated Webspark Panels Styles, WYSIWYG modules (misc. widget and WYSIWYG settings)

---
+++ profiles/openasu/modules/custom/webspark_featurescustom/webspark_panels_styles/plugins/styles/customstyles/js/customstyles.js
@@ -0,0 +1,26 @@
+/**
+ * JavaScript file for Webspark Panels Styles: Custom Styles module.
+ *
+ */
+
+(function ($, Drupal) {
+	Drupal.behaviors.customstyles = {
+		attach: function(context, settings) {
+		
+			/* Adding row-full and bagcolor class for rows with 2 or more columns */			
+			var a = document.getElementsByClassName('js-lightgraybg');
+			doIt(a,'lightgraybg');
+			var b = document.getElementsByClassName('js-darkgraybg');
+			doIt(b,'darkgraybg');
+			function doIt(a,color){
+				for(i=0;i<a.length;i++){
+					if(!$(a[i]).closest('.row').hasClass('row-full')){
+						$(a[i]).closest('.row').addClass('row-full '+color).wrapInner('<div class="pane-content"></div>');
+					}
+				}
+			}
+		
+		}
+	};
+})(jQuery, Drupal);
+
