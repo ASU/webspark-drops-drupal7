367713db2994b209c905e581d828e8d4ef8feea0
Bryan <aubjr@asu.edu>
2015-02-09 15:50:25 -0700

    Oklahoma II - Udpated Webspark Hero feature (previews of background images)

--- profiles/openasu/modules/custom/webspark_featurescustom/webspark_hero/js/webspark_hero.js
+++ profiles/openasu/modules/custom/webspark_featurescustom/webspark_hero/js/webspark_hero.js
@@ -1,15 +1,37 @@
 /**
- * JavaScript file for Webspark Hero module.
- *
- */
+* JavaScript file for Webspark Hero module.
+*
+*/
 
 (function ($, Drupal) {
 	Drupal.behaviors.webspark_hero = {
 		attach: function(context, settings) {
-		
-		  $(".pane-bundle-hero").wrapInner( "<div class='hero-wrapper'></div>");
-		  $(".pane-bundle-hero").addClass( "hero-bg-img");
-		  
+
+			$(".pane-bundle-hero").wrapInner( "<div class='hero-wrapper'></div>");
+			$(".pane-bundle-hero").addClass( "hero-bg-img");
+
+
+
+			$(function() {
+					$('.form-item-field-webspark-hero-bgimg-und-0 :file').change(function() {
+
+							if (this.files && this.files[0]) {
+									var reader = new FileReader();
+									reader.onload = imageIsLoaded;
+									reader.readAsDataURL(this.files[0]);
+							}
+					});
+			});
+
+				//adds background image to preview
+			function imageIsLoaded(e){
+					$("#panopoly-form-widget-preview").before("<style> .hero-bg-img {background-image: url("+e.target.result+");}</style>");
+
+			};
+
+
+
+
 		}
 	};
 })(jQuery, Drupal);
