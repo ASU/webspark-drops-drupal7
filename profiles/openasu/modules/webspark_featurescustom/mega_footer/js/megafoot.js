4fc047c8540c494254f8ae9308f2e4dab8f77aef
ctestama <ctestama@asu.edu>
2016-02-12 15:59:23 -0700

    WEBSPARK-771 - Deprecated fields in mega footer and alt text

--- profiles/openasu/modules/webspark_featurescustom/mega_footer/js/megafoot.js
+++ profiles/openasu/modules/webspark_featurescustom/mega_footer/js/megafoot.js
@@ -1,24 +1,24 @@
 (function ($, Drupal) {
-	Drupal.behaviors.webspark_mega_footer = {
-		attach: function(context, settings) {
+    Drupal.behaviors.webspark_mega_footer = {
+        attach: function (context, settings) {
 
-				if ($(window).innerWidth() >= 768) {
-						$('.big-foot-nav').not('.in').addClass('in');
-						$('.big-foot-head').removeAttr("data-toggle");
-				} else {
-					$('.big-foot-nav').removeClass('in');
-				}
+            if ($(window).innerWidth() >= 768) {
+                $('.big-foot-nav').not('.in').addClass('in');
+                $('.big-foot-head').removeAttr("data-toggle");
+            } else {
+                $('.big-foot-nav').removeClass('in');
+            }
 
-				$(window).resize(function() {
-						if ($(window).innerWidth() >= 768) {
-								$('.big-foot-nav').not('.in').addClass('in');
-								$('.big-foot-head').removeAttr("data-toggle");
-					} else {
-								$('.big-foot-nav').removeClass('in');
-								$('.big-foot-head').attr("data-toggle","collapse");
-					}
+            $(window).resize(function () {
+                if ($(window).innerWidth() >= 768) {
+                    $('.big-foot-nav').not('.in').addClass('in');
+                    $('.big-foot-head').removeAttr("data-toggle");
+                } else {
+                    $('.big-foot-nav').removeClass('in');
+                    $('.big-foot-head').attr("data-toggle", "collapse");
+                }
 
-				});
-		}
-	};
-})   (jQuery, Drupal);
+            });
+        }
+    };
+})(jQuery, Drupal);
