e61dd61355a816d5c4d6b336d94ab78dffc8c624
Jared Rounsavall <jaredrounsavall@macbook-pro.dhcp.asu.edu>
2016-03-04 11:10:38 -0700

    removing button altogether

--- profiles/openasu/themes/innovation/templates/block--tb-megamenu--main-menu.tpl.php
+++ profiles/openasu/themes/innovation/templates/block--tb-megamenu--main-menu.tpl.php
@@ -4,14 +4,17 @@
         $section = '';
     }
     if($section == 'frontend') :?>
-    <button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar tb-megamenu-button" type="button">
-      <i class="icon-reorder">=</i>
-    </button>
+
     <div class="nav-collapse collapse<?php print $block_config['always-show-submenu'] ? ' always-show' : '';?>">
   <?php endif;?>
   <?php
-        $tempContent = str_replace('<i class="icon-reorder"></i>','<i class="icon-reorder">=</i>',$content);
-        
+      
+        $tempContent = str_replace(
+    '<button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar tb-megamenu-button" type="button">
+      <i class="icon-reorder"></i>
+    </button>'
+          ,'',$content);
+
         print $tempContent;
         ?>
   <?php if($section == 'frontend') :?>
