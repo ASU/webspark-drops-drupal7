bfee9d4f64f638f786d0f01deb1c9ca7d8c4e98c
Bryan <aubjr@asu.edu>
2016-05-09 15:41:04 -0700

    WEBSPARK-420 (Radix) - Webb TPL update

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_webb/radix-webb.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_webb/radix-webb.tpl.php
@@ -3,6 +3,11 @@
  * @file
  * Template for Radix Webb.
  *
+ * Patched to support the classes and tags from the old Panopoly Webb for Webspark.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
+ *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
@@ -11,44 +16,47 @@
 ?>
 
 <div class="panel-display webb clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12 radix-layouts-header panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['header']; ?>
-        </div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-md-4 radix-layouts-sidebar panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['sidebar']; ?>
-        </div>
-      </div>
-      <div class="col-md-8 panel-panel">
-        <div class="row">
-          <div class="col-md-12 radix-layouts-contentheader panel-panel">
-            <div class="panel-panel-inner">
-              <?php print $content['contentheader']; ?>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6 radix-layouts-contentcolumn1 panel-panel">
-            <div class="panel-panel-inner">
-              <?php print $content['contentcolumn1']; ?>
-            </div>
-          </div>
-          <div class="col-md-6 radix-layouts-contentcolumn2 panel-panel">
-            <div class="panel-panel-inner">
-              <?php print $content['contentcolumn2']; ?>
-            </div>
+  <section class="section">
+    <div class="container">
+      <div class="row row-full">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> webb-header-area radix-layouts-header panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['header']; ?>
           </div>
         </div>
       </div>
     </div>
-  </div>
-
+  </section>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD); ?> webb-sidebar-area radix-layouts-sidebar panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['sidebar']; ?>
+          </div>
+        </div>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD * 2); ?> webb-flipped-column-content-region-area panel-panel">
+          <div class="row">
+            <div class="col-md--<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> webb-flipped-column-header-region-area radix-layouts-contentheader panel-panel">
+              <div class="panel-panel-inner">
+                <?php print $content['contentheader']; ?>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> webb-flipped-column-content-region-1 radix-layouts-contentcolumn1 panel-panel">
+              <div class="panel-panel-inner">
+                <?php print $content['contentcolumn1']; ?>
+              </div>
+            </div>
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> webb-flipped-column-content-region-2 radix-layouts-contentcolumn2 panel-panel">
+              <div class="panel-panel-inner">
+                <?php print $content['contentcolumn2']; ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.webb-column-content -->
+  </section>
 </div><!-- /.webb -->
