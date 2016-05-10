cfb75ecdcedec63317e0a16c6680e9db85d8a27c
Kathy Marks <kathymarks@kathys-mbp.mobile.asu.edu>
2016-05-10 16:06:27 -0700

    WEBSPARK-420 (Radix) - Sutro

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_sutro/radix-sutro.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_sutro/radix-sutro.tpl.php
@@ -7,40 +7,55 @@
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
  * panel of the layout. This layout supports the following sections:
+ *
+ * Patched to support the classes and tags from the old, comparably named Panopoly layout for Webspark.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
  */
 ?>
 
 <div class="panel-display sutro clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12 radix-layouts-header panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['header']; ?>
-        </div>
+  <section class="section">
+    <div class="container">
+      <div class="row row-full">
+				<div class="col-md-12 radix-layouts-header panel-panel col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> sutro-header-area">
+					<div class="panel-panel-inner">
+						<?php print $content['header']; ?>
+					</div>
+				</div>
       </div>
     </div>
+  </section>
     
-    <div class="row">
-      <div class="col-md-6 radix-layouts-column1 panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['column1']; ?>
-        </div>
-      </div>
-      <div class="col-md-6 radix-layouts-column2 panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['column2']; ?>
-        </div>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+				<div class="col-md-6 radix-layouts-column1 panel-panel col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> sutro-column-content-region-1">
+					<div class="panel-panel-inner">
+						<?php print $content['column1']; ?>
+					</div>
+				</div>
+				<div class="col-md-6 radix-layouts-column2 panel-panel col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> sutro-column-content-region-2">
+					<div class="panel-panel-inner">
+						<?php print $content['column2']; ?>
+					</div>
+				</div>
       </div>
     </div>
+  </section>
     
-    <div class="row">
-      <div class="col-md-12 radix-layouts-footer panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['footer']; ?>
-        </div>
+  <footer class="section" id="footer">
+    <div class="container">
+      <div class="row row-full">
+				<div class="col-md-12 radix-layouts-footer panel-panel col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> sutro-footer-area">
+					<div class="panel-panel-inner">
+						<?php print $content['footer']; ?>
+					</div>
+				</div>
       </div>
     </div>
-  </div>
+  </footer>
  
 </div><!-- /.sutro -->
