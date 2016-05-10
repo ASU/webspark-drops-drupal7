93e042f38a5a32851f164ce12a74cd8300cbac16
Bryan <aubjr@asu.edu>
2016-05-10 15:41:56 -0700

    WEBSPARK-420 (Radix) - Geary

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_geary/radix-geary.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_geary/radix-geary.tpl.php
@@ -3,6 +3,11 @@
  * @file
  * Template for Radix Geary.
  *
+ * Patched to support the classes and tags from the old, comparably named Panopoly layout for Webspark.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
+ *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
@@ -11,33 +16,36 @@
 ?>
 
 <div class="panel-display geary clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12 radix-layouts-header panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['header']; ?>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> geary-main-container radix-layouts-header panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['header']; ?>
+          </div>
         </div>
       </div>
     </div>
-    
-    <div class="row">
-      <div class="col-md-4 radix-layouts-column1 panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['column1']; ?>
+  </section>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD, 3); ?> geary-column-content-region-1 radix-layouts-column1 panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['column1']; ?>
+          </div>
         </div>
-      </div>
-      <div class="col-md-4 radix-layouts-column2 panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['column2']; ?>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD, 3); ?> geary-column-content-region-2 radix-layouts-column2 panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['column2']; ?>
+          </div>
         </div>
-      </div>
-      <div class="col-md-4 radix-layouts-column3 panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['column3']; ?>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD, 3); ?> geary-column-content-region-3 radix-layouts-column3 panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['column3']; ?>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  
+  </section>
 </div><!-- /.geary -->
