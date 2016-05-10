bd8e55f7565b2f1d92be0128a2eeec93652354c9
Bryan <aubjr@asu.edu>
2016-05-10 15:32:41 -0700

    WEBSPARK-420 (Radix) - Bryant Flipped

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_bryant_flipped/radix-bryant-flipped.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_bryant_flipped/radix-bryant-flipped.tpl.php
@@ -3,6 +3,11 @@
  * @file
  * Template for Radix Bryant Flipped.
  *
+ * Patched to support the classes and tags from the old, comparably named Panopoly layout for Webspark.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
+ *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
@@ -11,20 +16,20 @@
 ?>
 
 <div class="panel-display bryant-flipped clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-9 radix-layouts-content panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['contentmain']; ?>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FOURTH * 3); ?> bryant-flipped-content-region radix-layouts-content panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['contentmain']; ?>
+          </div>
         </div>
-      </div>
-      <div class="col-md-3 radix-layouts-sidebar panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['sidebar']; ?>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FOURTH); ?> bryant-flipped-sidebar-region radix-layouts-sidebar panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['sidebar']; ?>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-
+  </section>
 </div><!-- /.bryant-flipped -->
