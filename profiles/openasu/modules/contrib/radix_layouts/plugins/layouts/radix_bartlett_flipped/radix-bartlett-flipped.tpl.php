4822c678df0c5323b86518a9918dd22726c2c78b
Bryan <aubjr@asu.edu>
2016-05-10 14:45:19 -0700

    WEBSPARK-420 (Radix) - Bartlett Flipped

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_bartlett_flipped/radix-bartlett-flipped.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_bartlett_flipped/radix-bartlett-flipped.tpl.php
@@ -11,36 +11,36 @@
 ?>
 
 <div class="panel-display bartlett-flipped clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="container-fluid">
-    <div class="row">
-
-      <div class="col-md-8 panel-panel">
-        <div class="row">
-          <div class="col-md-12 radix-layouts-contentheader panel-panel">
-            <div class="panel-panel-inner">
-              <?php print $content['contentheader']; ?>
+  <section class="section">
+    <div class="container">
+      <div class="row row-full">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD * 2); ?> bartlett-flipped-main panel-panel">
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> bartlett-flipped-main-header radix-layouts-contentheader panel-panel">
+              <div class="panel-panel-inner">
+                <?php print $content['contentheader']; ?>
+              </div>
             </div>
           </div>
-          <div class="col-md-6 radix-layouts-contentcolumn1 panel-panel">
-            <div class="panel-panel-inner">
-              <?php print $content['contentcolumn1']; ?>
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> bartlett-flipped-main-col-1 radix-layouts-contentcolumn1 panel-panel">
+              <div class="panel-panel-inner">
+                <?php print $content['contentcolumn1']; ?>
+              </div>
             </div>
-          </div>
-          <div class="col-md-6 radix-layouts-contentcolumn2 panel-panel">
-            <div class="panel-panel-inner">
-              <?php print $content['contentcolumn2']; ?>
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> bartlett-flipped-main-col-2 radix-layouts-contentcolumn2 panel-panel">
+              <div class="panel-panel-inner">
+                <?php print $content['contentcolumn2']; ?>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <!-- Sidebar -->
-      <div class="col-md-4 radix-layouts-sidebar panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['sidebar']; ?>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD); ?> bartlett-flipped-side radix-layouts-sidebar panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['sidebar']; ?>
+          </div>
         </div>
       </div>
-
-    </div>
-  </div>  
+    </div><!-- /.container -->
+  </section><!--  /.section -->
 </div><!-- /.bartlett-flipped -->
