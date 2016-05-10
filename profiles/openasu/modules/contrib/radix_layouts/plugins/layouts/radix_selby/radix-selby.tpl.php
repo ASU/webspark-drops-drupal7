795ac51612932a813ee34801507cb6c1a2932eec
Bryan <aubjr@asu.edu>
2016-05-10 16:29:15 -0700

    WEBSPARK-420 (Radix) - Selby

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_selby/radix-selby.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_selby/radix-selby.tpl.php
@@ -3,6 +3,11 @@
  * @file
  * Template for Radix Selby.
  *
+ * Patched to support the classes and tags from the old, comparably named Panopoly layout for Webspark.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
+ *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
@@ -11,35 +16,35 @@
 ?>
 
 <div class="panel-display selby clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-4 radix-layouts-sidebar panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['sidebar']; ?>
-        </div>
-      </div>
-      <div class="col-md-8 panel-panel">
-        <div class="row">
-          <div class="col-md-12 radix-layouts-contentheader panel-panel">
-            <?php print $content['contentheader']; ?>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD); ?> selby-sidebar-main-area radix-layouts-sidebar panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['sidebar']; ?>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-6 radix-layouts-contentcolumn1 panel-panel">
-            <?php print $content['contentcolumn1']; ?>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD * 2); ?> selby-column-content-region-area panel-panel">
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> selby-column-content-region-area radix-layouts-contentheader panel-panel">
+              <?php print $content['contentheader']; ?>
+            </div>
           </div>
-          <div class="col-md-6 radix-layouts-contentcolumn2 panel-panel">
-            <?php print $content['contentcolumn2']; ?>
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> selby-column-content-region-1 radix-layouts-contentcolumn1 panel-panel">
+              <?php print $content['contentcolumn1']; ?>
+            </div>
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> selby-column-content-region-2 radix-layouts-contentcolumn2 panel-panel">
+              <?php print $content['contentcolumn2']; ?>
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 radix-layouts-contentfooter panel-panel">
-            <?php print $content['contentfooter']; ?>
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> selby-content-footer-area radix-layouts-contentfooter panel-panel">
+              <?php print $content['contentfooter']; ?>
+            </div>
           </div>
-        </div>
-      </div>
+        </div><!-- /.selby-content-container -->
+      </div><!-- /.selby-content-container row-->
     </div>
-  </div>
-
+  </section>
 </div><!-- /.selby -->
