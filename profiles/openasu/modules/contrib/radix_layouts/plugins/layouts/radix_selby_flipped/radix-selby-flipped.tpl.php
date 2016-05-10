5b39f8383621ed6f7641249f9c9691bd6aee0fa5
Bryan <aubjr@asu.edu>
2016-05-10 16:40:30 -0700

    WEBSPARK-420 (Radix) - Selby Flipped

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_selby_flipped/radix-selby-flipped.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_selby_flipped/radix-selby-flipped.tpl.php
@@ -3,6 +3,11 @@
  * @file
  * Template for Radix Selby Flipped.
  *
+ * Patched to support the classes and tags from the old, comparably named Panopoly layout for Webspark.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
+ *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
@@ -10,36 +15,36 @@
  */
 ?>
 
-<div class="panel-display selby-flipped clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-8 panel-panel">
-        <div class="row">
-          <div class="col-md-12 radix-layouts-contentheader panel-panel">
-            <?php print $content['contentheader']; ?>
+<div class="panel-display selby-flipped selby-flipped-flipped-flipped clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD * 2); ?> selby-flipped-column-content-region-area panel-panel">
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> selby-flipped-column-content-region-area radix-layouts-contentheader panel-panel">
+              <?php print $content['contentheader']; ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> selby-flipped-column-content-region-1 radix-layouts-contentcolumn1 panel-panel">
+              <?php print $content['contentcolumn1']; ?>
+            </div>
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF); ?> selby-flipped-column-content-region-2 radix-layouts-contentcolumn2 panel-panel">
+              <?php print $content['contentcolumn2']; ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FULL); ?> selby-flipped-content-footer-area radix-layouts-contentfooter panel-panel">
+              <?php print $content['contentfooter']; ?>
+            </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-6 radix-layouts-contentcolumn1 panel-panel">
-            <?php print $content['contentcolumn1']; ?>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_THIRD); ?> selby-flipped-sidebar-main-area radix-layouts-sidebar panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['sidebar']; ?>
           </div>
-          <div class="col-md-6 radix-layouts-contentcolumn2 panel-panel">
-            <?php print $content['contentcolumn2']; ?>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 radix-layouts-contentfooter panel-panel">
-            <?php print $content['contentfooter']; ?>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4 radix-layouts-sidebar panel-panel">
-        <div class="panel-panel-inner">
-          <?php print $content['sidebar']; ?>
         </div>
       </div>
     </div>
-  </div>
-
-</div><!-- /.selby-flipped -->
+  </section>
+</div><!-- /.selby-flipped --><!-- /.selby-flipped-flipped-flipped -->
