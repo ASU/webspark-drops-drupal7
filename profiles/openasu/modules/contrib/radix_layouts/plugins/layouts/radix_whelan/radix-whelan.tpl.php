5937b124f9f2125782800e40183bc7aa0fabd9e4
Bryan <aubjr@asu.edu>
2016-05-10 13:42:00 -0700

    WEBSPARK-420 (Radix) - Whelan

--- profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_whelan/radix-whelan.tpl.php
+++ profiles/openasu/modules/contrib/radix_layouts/plugins/layouts/radix_whelan/radix-whelan.tpl.php
@@ -3,6 +3,12 @@
  * @file
  * Template for Radix Whelan.
  *
+ * Patched to support the classes and tags from the old Panopoly Whelan for Webspark.
+ * Left out extra -flipped bug this time.
+ *
+ * Functions:
+ * kalatheme_grid_size (from kalatheme theme) - returns (int) Number of Bootstrap columns.
+ *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
@@ -11,25 +17,25 @@
 ?>
 
 <div class="panel-display whelan clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-md-3 radix-layouts-column1 panel-panel">
-      <div class="panel-panel-inner">
-        <?php print $content['column1']; ?>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FOURTH, 3); ?> whelan-column-content-region-1 radix-layouts-column1 panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['column1']; ?>
+          </div>
+        </div>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_HALF, 3); ?> whelan-column-content-region-2 radix-layouts-content panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['contentmain']; ?>
+          </div>
+        </div>
+        <div class="col-md-<?php print kalatheme_grid_size(KALATHEME_GRID_FOURTH, 3); ?> whelan-column-content-region-3 radix-layouts-column2 panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['column2']; ?>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="col-md-6 radix-layouts-content panel-panel">
-      <div class="panel-panel-inner">
-        <?php print $content['contentmain']; ?>
-      </div>
-    </div>
-    <div class="col-md-3 radix-layouts-column2 panel-panel">
-      <div class="panel-panel-inner">
-        <?php print $content['column2']; ?>
-      </div>
-    </div>
-  </div>
-</div>
-  
+  </section>
 </div><!-- /.whelan -->
