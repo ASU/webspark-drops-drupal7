41db8e5fbf947e5782648994eeecd8b308269ef8
Bryan <aubjr@asu.edu>
2016-08-04 14:27:36 -0700

    WEBSPARK-919 - Updated TB Megamenu to 1.0-rc2

--- profiles/openasu/modules/contrib/tb_megamenu/templates/tb-megamenu.tpl.php
+++ profiles/openasu/modules/contrib/tb_megamenu/templates/tb-megamenu.tpl.php
@@ -1,9 +1,9 @@
 <div <?php print $attributes;?> class="<?php print $classes;?>">
   <?php if($section == 'frontend') :?>
     <button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar tb-megamenu-button" type="button">
-      <i class="icon-reorder"></i>
+      <i class="fa fa-reorder"></i>
     </button>
-    <div class="nav-collapse collapse<?php print $block_config['always-show-submenu'] ? ' always-show' : '';?>">
+    <div class="nav-collapse <?php print $block_config['always-show-submenu'] ? ' always-show' : '';?>">
   <?php endif;?>
   <?php print $content;?>
   <?php if($section == 'frontend') :?>
