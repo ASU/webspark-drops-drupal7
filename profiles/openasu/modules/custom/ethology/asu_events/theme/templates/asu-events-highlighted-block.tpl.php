a8ec9ed8fd6eaa9bc9d5ad0f721aa6752a0bc4fe
Bryan <aubjr@asu.edu>
2017-02-14 17:15:19 -0700

    Removed Ethology modules for now

--- profiles/openasu/modules/custom/ethology/asu_events/theme/templates/asu-events-highlighted-block.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * ASU Events Full block tpl
- **/
-
-?>
-<div <?php print $attributes;?> class="<?php print $class . " " . $classes;?> row">
-  <?php print render($events_items); ?>
-</div>
--- profiles/openasu/modules/custom/ethology/asu_news/theme/templates/asu-news-highlighted-block.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * ASU News Full block tpl
- **/
-
-?>
-<div <?php print $attributes;?> class="<?php print $classes;?> row">
-  <?php print render($news_items); ?>
-</div>
