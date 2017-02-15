7bef5826f008f57c0b22d8917aebe7dda2dc1ad5
Bryan <aubjr@asu.edu>
2017-02-14 17:01:13 -0700

    Removing ASU Dirs module for now from master branch

--- profiles/openasu/modules/custom/asu_dirs/asu_isearch/theme/fieldable-panels-pane--isearch-directory.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * ASU iSearch directory pane
- */
-?>
-<div
-  class="asu-isearch-directory-pane <?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($content); ?>
-</div>
