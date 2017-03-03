967451f08014029e220cd70483198e9452796a8a
Bryan <aubjr@asu.edu>
2017-03-03 13:04:46 -0700

    WEBSPARK-1073 - Webspark Seven theme cleanup (removed JS code from Content Callout feature)

---
+++ profiles/openasu/themes/webspark_seven/js/webspark_seven.js
@@ -0,0 +1,28 @@
+/**
+ * Webspark Seven theme JS (a subtheme of Drupal Core's Seven theme).
+ *
+ */
+
+(function ($, Drupal) {
+    Drupal.behaviors.websparkSeven = {
+        attach: function(context, settings) {
+
+            // Show/hide the media inputs on content callouts add form
+            $("select#edit-field-callout-media-und").change(function(){
+                $( "select#edit-field-callout-media-und option:selected").each(function(){
+
+                    if($(this).attr("value")=="image"){
+                        $(".group-callout-opts .field-type-video-embed-field").hide();
+                        $(".group-callout-opts .field-type-image").show();
+                    }
+                    if($(this).attr("value")=="video"){
+                        $(".group-callout-opts .field-type-image").hide();
+                        $(".group-callout-opts .field-type-video-embed-field").show();
+                    }
+                });
+            }).change();
+
+        }
+    };
+})(jQuery, Drupal);
+
