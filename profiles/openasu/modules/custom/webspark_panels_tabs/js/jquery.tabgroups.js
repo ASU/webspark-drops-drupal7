26053a987a7d8f9bded9fbd93a2c0ad8182c7c9e
ctestama <ctestama@asu.edu>
2017-05-19 10:38:52 -0700

    Webspark-1110 - added tab focus for accessiblity

--- profiles/openasu/modules/custom/webspark_panels_tabs/js/jquery.tabgroups.js
+++ profiles/openasu/modules/custom/webspark_panels_tabs/js/jquery.tabgroups.js
@@ -54,6 +54,7 @@
                                 if (self.curgrp != j) {
                                     self.curgrp = j;
                                     self.activateGroup();
+                                    ui.newTab.focus();
                                 }
 
                                 return;
