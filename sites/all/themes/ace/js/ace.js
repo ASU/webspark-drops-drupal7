1e135b72973f0ab7ddf868e1e1caa072d65b3bf8
Bryan <aubjr@asu.edu>
2017-06-24 08:42:50 -0700

    NOJIRA - Updated theme and data structure for Task Force meetings

--- sites/all/themes/ace/js/ace.js
+++ sites/all/themes/ace/js/ace.js
@@ -1,17 +1,14 @@
 /**
- * Created by bryan on 6/23/17.
- */
-
-/**
  * JavaScript file for theme.
  *
  */
 (function ($, Drupal) {
-  Drupal.behaviors.asu_standard = {
+  Drupal.behaviors.aceSubTheme = {
     attach: function (context, settings) {
 
-      $("p#simple-gmap-address-link").click(function() {
-        $(this).siblings("iframe").toggle("400");
+      $("p#simple-gmap-address-link").css({"text-decoration":"underline", "color":"#8c1d40"})
+        .click(function() {
+         $(this).siblings("iframe").toggle("300");
       });
 
     }
