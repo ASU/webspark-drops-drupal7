e183bdac8afc755e5ce268f83b4a091a7f9c38f6
Bryan <aubjr@asu.edu>
2017-06-24 19:21:40 -0700

    Fixed JS issue (perpetual reloading)

--- sites/all/themes/ace/template.php
+++ sites/all/themes/ace/template.php
@@ -2,12 +2,4 @@
 /** 
  * @file 
  * Primary pre/preprocess functions and alterations.
- */ 
-
-function ace_preprocess_page(&$variables) {
-  if (isset($variables['node']->type)) {
-    if ($variables['node']->type == "ace_task_force_meeting") {
-      drupal_add_js(path_to_theme() . "/js/ace.js");
-    }
-  }
-}
+ */
