8d618c0084a0ffb124d022afa4ef64ece34141e8
Bryan <aubjr@asu.edu>
2018-01-31 16:09:59 -0700

    WEBSPARK-1234 - Webspark Jumbohero: Added SCSS + Updated CSS (ems units, added ellipsis to long blurbs

--- profiles/openasu/modules/webspark_featurescustom/webspark_jumbohero/templates/fieldable-panels-pane--jumbohero.tpl.php
+++ profiles/openasu/modules/webspark_featurescustom/webspark_jumbohero/templates/fieldable-panels-pane--jumbohero.tpl.php
@@ -1,5 +1,7 @@
 <div class="jumbotext-wrapper position-<?php print render($content['field_webspark_jumbo_position']['#items'][0]['value']); ?> <?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print render($elements['title']); ?>
+  <?php // Title only addition to default fieldable-panels-pane.tpl.php; Same as webspark banner TPL.
+  print render($elements['title']);
+  ?>
   <?php print render($title_suffix); ?>
   <?php print render($content); ?>
 </div>
