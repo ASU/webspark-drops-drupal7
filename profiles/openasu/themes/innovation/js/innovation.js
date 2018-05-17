9b3598f2e26aaaecb1de4e3d7f7798c11b2d0d3c
Michael Gilardi <mgilardi@asu.edu>
2018-05-16 22:36:27 -0700

    WEBSPARK-1300 - Fix Mega Menu sticky menu behavior with other elements; add sticky menu support for additional browsers

--- profiles/openasu/themes/innovation/js/innovation.js
+++ profiles/openasu/themes/innovation/js/innovation.js
@@ -14,6 +14,7 @@
 
     }
   };
+
   // Add support for sticky menu
   var navOffset = 0;
   $(window).scroll(function () {
@@ -24,8 +25,12 @@
       }
     }
     navOffsetUnits = navOffset + "px";
-    var navStyles = {"position": "sticky", "top": navOffsetUnits, "z-index": "1000", "width": "100%", "visibility": "visible"};
-    $('#ASUNavMenu').addClass('sticky-menu').css(navStyles);
+    var navOffsetTop = {"top": navOffsetUnits};
+    if (($('#ASUNavMenu').offset().top - ($(window).scrollTop() + navOffset)) < 1 && $('.sticky-menu').length < 1) {
+      $('#ASUNavMenu').addClass('sticky-menu').css(navOffsetTop);
+    } else if (($('#ASUNavMenu').offset().top - ($(window).scrollTop() + navOffset)) >= 1) {
+      $('#ASUNavMenu').removeClass('sticky-menu').removeAttr("style");
+    }
   });
 
   // Used to determine if RFI form is on the page.
