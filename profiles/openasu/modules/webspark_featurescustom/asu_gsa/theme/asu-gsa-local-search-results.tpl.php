e07069e1e2bc627b23fed1bded4668eb007801b5
Bryan <aubjr@asu.edu>
2018-10-05 20:19:05 -0700

    WEBSPARK-1410 - Fixes multiple issues in ASU GSA module (theming, TPLs)

---
+++ profiles/openasu/modules/webspark_featurescustom/asu_gsa/theme/asu-gsa-local-search-results.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for displaying search results.
+ *
+ * This template collects each invocation of theme_search_result(). This and
+ * the child template are dependent to one another sharing the markup for
+ * definition lists.
+ *
+ * Note that modules may implement their own search type and theme function
+ * completely bypassing this template.
+ *
+ * Available variables:
+ * - $search_results: All results as it is rendered through
+ *   search-result.tpl.php
+ * - $module: The machine-readable name of the module (tab) being searched, such
+ *   as "node" or "user".
+ *
+ *
+ * @see template_preprocess_search_results()
+ *
+ * @ingroup themeable
+ */
+?>
+<div class="asu-gsa-search-results asu-gsa--local--search-results">
+<?php if ($search_results): ?>
+  <h2 id="search-results-heading"><?php print t('Search results');?></h2>
+  <ol class="search-results <?php print $module; ?>-results">
+    <?php print $search_results; ?>
+  </ol>
+  <?php print $pager; ?>
+<?php else : ?>
+  <h4 id="search-results-none"><?php print t('Your search yielded no results.');?></h4>
+  <?php print search_help('search#noresults', drupal_help_arg()); ?>
+<?php endif; ?>
+</div>
