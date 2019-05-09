c90375ad6bd7620a8c35927b53878629bfd69ed8
Bryan <aubjr@asu.edu>
2019-05-09 10:59:15 -0700

    WEBSPARK-1511 (and another past ticket) - Remove experimental ASU News + Events modules

--- profiles/openasu/modules/custom/asu_news/theme/templates/asu-news-highlighted-block.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * ASU News Full block tpl
- **/
-
-?>
-<div <?php print $attributes;?> class="<?php print $classes;?> row highlighted-wrapper">
-  <?php print render($news_items); ?>
-</div>
