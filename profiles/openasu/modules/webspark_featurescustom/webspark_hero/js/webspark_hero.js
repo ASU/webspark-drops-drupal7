ae85687f3f578bffa93cbbef513e1f6ef85a6907
Bryan <aubjr@asu.edu>
2019-09-25 14:54:31 -0700

    WEBSPARK-1393 - Removed Ellipsis from Hero, Jumbo Hero FPPs when blurb is too long

--- profiles/openasu/modules/webspark_featurescustom/webspark_hero/js/webspark_hero.js
+++ profiles/openasu/modules/webspark_featurescustom/webspark_hero/js/webspark_hero.js
@@ -10,7 +10,6 @@
 
       $(function () {
         $('.form-item-field-webspark-hero-bgimg-und-0 :file').change(function () {
-
           if (this.files && this.files[0]) {
             var reader = new FileReader();
             reader.onload = imageIsLoaded;
