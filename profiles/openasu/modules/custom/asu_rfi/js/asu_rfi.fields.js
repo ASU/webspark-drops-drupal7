9292224edc8dd278330c946b05e42f3e71446ba3
Bryan R <aubjr@asu.edu>
2019-11-06 16:52:04 -0700

    WEBSPARK-1550 - MAJOR refactoring of .module file (see below); Added Test Mode check; More Refactoring in SF Post file; - Major re-ordering of the code (in .module file) - Combined form validation code for matching form elements (birthdate, etc.) into callbacks for all forms - Moved JS into theme function; Updated function comments for "which form is which" help; Combined form init settings code; WEBSPARK-1550 - Added UNDERGRAD, GRAD constants; Added dash to mobile number; Javascript enterpriseclientID, clientid, trackingid work

--- profiles/openasu/modules/custom/asu_rfi/js/asu_rfi.fields.js
+++ profiles/openasu/modules/custom/asu_rfi/js/asu_rfi.fields.js
@@ -1,23 +1,28 @@
 (function ($) {
-  // Give GA time to load.
-  setTimeout(function () {
-    // Set the EdPlus Lead API enterpriseclientid if possible.
-    if ('undefined' != typeof ga) {
-      ga(function () {
-        //var extraFields = "";
-        var gaIds = ga.getAll();
-        var i, size, match;
-        for (i = 0, size = gaIds.length, match = 0; i < size; i++) {
-          if (gaIds[i].get('trackingId') === 'UA-42798992-4') {
-            //The field name sent to the Lead API should be enterpriseclientid
-            //You can use an existing field or create it dynamically
-            $("input[name=enterpriseclientid]").val(gaIds[i].get('clientId'));
-            //This counter should be equals to the number of GA accounts to be read
-            if (++match === 1)
-              break;
-          }
+  Drupal.behaviors.asuRfiFields = {
+    attach: function (context, settings) {
+      // Give GA time to load.
+      setTimeout(function () {
+        // Set the EdPlus Lead API enterpriseclientid if possible.
+        if ('undefined' != typeof ga) {
+          ga(function () {
+            //var extraFields = "";
+            var gaIds = ga.getAll();
+            var i, size, match;
+            for (i = 0, size = gaIds.length, match = 0; i < size; i++) {
+              if (gaIds[i].get('trackingId') === 'UA-42798992-4') {
+                //The field name sent to the Lead API should be enterpriseclientid
+                //You can use an existing field or create it dynamically
+                var ga_id = gaIds[i].get('clientId');
+                $('input[name="enterpriseclientid"]').val(ga_id);
+                // This counter should be equal to the number of GA accounts to be read
+                if (++match === 1)
+                  break;
+              }
+            }
+          });
         }
-      });
+      }, 5000);
     }
-  }, 5000);
+  }
 })(jQuery);
