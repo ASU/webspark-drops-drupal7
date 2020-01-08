bcba55b0e872fa3d6bb636b0b3f804a0152e069b
Bryan R <aubjr@asu.edu>
2020-01-08 14:32:26 -0700

    NOJIRA - Preparation for Zipperlandville release

--- profiles/openasu/themes/innovation/template.php
+++ profiles/openasu/themes/innovation/template.php
@@ -60,7 +60,7 @@ function innovation_preprocess_html(&$variables) {
       '#tag' => 'meta',
       '#attributes' => array(
         // Don't forget to update openasu.info as well!!
-        'content' => 'Webspark:1.70.6 (Fort Worth)',
+        'content' => 'Webspark:1.70.7 (Zipperlandville)',
         'http-equiv' => 'X-Name-of-Distro',
         'name' => 'cmsversion',
       )
