97a2bc06e3d6c741be6a9ec5e4f887077c16fc9e
Bryan R <aubjr@asu.edu>
2020-02-25 19:02:09 -0700

    WEBSPARK-1550 - Fixed Related programs logic

--- profiles/openasu/modules/custom/asu_ap/templates/page--asu-ap.tpl.php
+++ profiles/openasu/modules/custom/asu_ap/templates/page--asu-ap.tpl.php
@@ -611,7 +611,10 @@ if (module_exists('metatag')) {
   } ?>
           <div class="row">
             <div class="col-md-12" id="plan-of-study">
-  <?php if ($related_programs === TRUE): ?>
+  <?php
+  if ($related_programs === TRUE
+    && (isset($node_info['field_asu_ap_prog_req']['#items'][0]['safe_value'])
+    XOR isset($node_info['field_asu_ap_admission_req']['#items'][0]['safe_value']))): ?>
               <div class="col-md-4 col-sm-12 asu-ap-related-programs-solo">
     <?php
     print $related_programs_output;
@@ -623,7 +626,7 @@ if (module_exists('metatag')) {
               <h2>Program requirements</h2>
     <?php print $node_info['field_asu_ap_prog_req']['#items'][0]['safe_value']; ?>
   <?php elseif (isset($node_info['field_asu_ap_admission_req']['#items'][0]['safe_value'])): ?>
-              <h2>Program requirements</h2>
+              <h2>Admission requirements</h2>
     <?php print $node_info['field_asu_ap_admission_req']['#items'][0]['safe_value']; ?>
   <?php endif; ?>
             </div>
