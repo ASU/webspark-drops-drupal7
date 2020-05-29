56675f0c69df544817ae1eff029c184fb99e12f3
Bryan R <aubjr@asu.edu>
2020-05-28 22:28:34 -0700

    Webspark Release - Texon (v1.70.10) - Please review the release notes at webspark.asu.edu before applying!

--- profiles/openasu/themes/innovation/template.php
+++ profiles/openasu/themes/innovation/template.php
@@ -60,7 +60,7 @@ function innovation_preprocess_html(&$variables) {
       '#tag' => 'meta',
       '#attributes' => array(
         // Don't forget to update openasu.info as well!!
-        'content' => 'Webspark:1.70.9 (Stanton)',
+        'content' => 'Webspark:1.70.10 (Texon)',
         'http-equiv' => 'X-Name-of-Distro',
         'name' => 'cmsversion',
       )
