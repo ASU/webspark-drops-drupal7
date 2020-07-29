65e79f5f92e726a13ad776d618213b8263db49a9
Bryan R <aubjr@asu.edu>
2020-07-28 18:55:25 -0700

    WEBSPARK-1683 - El Puente: Added TB Megamenu per-item TPL; Updated Font Awesome to Chevron (from caret); Cleaned out nav.scss related to navbar-nav class not found in this theme

--- profiles/openasu/themes/elpuente/templates/tb-megamenu.tpl.php
+++ profiles/openasu/themes/elpuente/templates/tb-megamenu.tpl.php
@@ -3,7 +3,7 @@
  * Override TB Megamenu module main TPL
  */
 ?>
-<div <?php print $attributes;?> class="<?php print $classes;?>" style="border: 1px red solid;">
+<div <?php print $attributes;?> class="<?php print $classes;?>">
   <?php if($section == 'frontend') :?>
     <button data-target=".nav-collapse" data-toggle="collapse" class="btn btn-navbar tb-megamenu-button" type="button">
       <i class="fa fa-reorder"></i>
