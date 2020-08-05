976a3073a02560acaa4e9a4fa520c137cd645574
Bryan R <aubjr@asu.edu>
2020-08-04 18:39:43 -0700

    WEBSPARK-1683 - El Puente: Refactored buttons.scss for megamenu buttons; Fixed CSS transitions

--- profiles/openasu/themes/elpuente/templates/tb-megamenu-item.tpl.php
+++ profiles/openasu/themes/elpuente/templates/tb-megamenu-item.tpl.php
@@ -1,3 +1,8 @@
+<?php
+if ($item['link']['title'] === 'Buttons-Links') {
+//  dpm(get_defined_vars());
+}
+?>
 <li <?php print $attributes;?> class="<?php print $classes;?>" aria-level="<?php print $level; ?>">
   <a href="<?php print in_array($item['link']['href'], array('<nolink>')) ? "#" : url($item['link']['href'], $item['link']['options']);?>" <?php echo drupal_attributes($item['link']['#attributes']); ?><?php if ($submenu): ?> aria-haspopup="true"<?php endif; ?>>
     <?php if(!empty($item_config['xicon'])) : ?>
