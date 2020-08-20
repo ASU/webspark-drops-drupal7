103032dc94aa05f95830ab29fa3b119a97fb7430
Bryan R <aubjr@asu.edu>
2020-08-19 17:10:09 -0700

    WEBSPARK-1658, 1682, etc. - Added ASU Brand permissions for a configuration form (for non-Webspark installs to manage version swapping of ASU Brand assets); Added framework for new WS2.0 blocks; Removed legacy header/footer field configuration options; Replaced calls to XML lists of header/footer versions with hard-coded list, along with version/compatibility checking. Refactored variable naming convention back to asu_brand_*; Moved inc files into own subdirectory; Updated documentation

--- profiles/openasu/modules/custom/asu_brand/asu_brand.js
+++ profiles/openasu/modules/custom/asu_brand/asu_brand.js
@@ -3,22 +3,20 @@
  */
 
 (function ($) {
+  Drupal.behaviors.asuBrandSetupHeader = {
+    attach: function (context, settings) {
+      $("body", context).once("asu_brand", function () {
+        // ASUHeader should already be initialized by the provided header files but we should verify. Also need to verify
+        // that the hostname is not listed in the override list, in which case we do not pass the /cas login path
+        var overrides = settings.asu_brand.overrides ? settings.asu_brand.overrides : [];
+        var hostname = window.location.hostname;
 
-Drupal.behaviors.asuBrandSetupHeader = {
-  attach: function (context, settings) {
-    $('body', context).once('asu_brand', function () {
-      // ASUHeader should already be initialized by the provided header files but we should verify. Also need to verify
-      // that the hostname is not listed in the override list, in which case we do not pass the /cas login path
-      var overrides = settings.asu_brand.overrides ? settings.asu_brand.overrides : [];
-      var hostname = window.location.hostname;
-
-      if (overrides.indexOf(hostname) == -1 && ASUHeader) {
-        ASUHeader.user_signedin = (settings.asu_brand.asu_sso_signedin === 'true');
-        ASUHeader.signin_url = settings.asu_brand.asu_sso_signinurl;
-        ASUHeader.signout_url = settings.asu_brand.asu_sso_signouturl;
-      }
-    });
-  }
-};
-
+        if (overrides.indexOf(hostname) == -1 && ASUHeader) {
+          ASUHeader.user_signedin = (settings.asu_brand.asu_sso_signedin === "true");
+          ASUHeader.signin_url = settings.asu_brand.asu_sso_signinurl;
+          ASUHeader.signout_url = settings.asu_brand.asu_sso_signouturl;
+        }
+      });
+    }
+  };
 })(jQuery);
