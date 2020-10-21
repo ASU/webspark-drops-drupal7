394244f71f395b875436e7fcee678371828c5849
Bryan R <aubjr@asu.edu>
2020-10-20 17:04:03 -0700

    WEBSPARK-1863 - Added base classes to College, Innovation HTML-level TPLs; Narrowed <a> tag CSS to non-button links

--- profiles/openasu/themes/college/templates/html.tpl.php
+++ profiles/openasu/themes/college/templates/html.tpl.php
@@ -58,7 +58,7 @@
   <?php print $scripts; ?>
 </head>
 
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<body class="<?php print $classes; ?> college" <?php print $attributes;?>>
 <?php print $page_top; ?>
 <?php print $page; ?>
 <?php print $page_bottom; ?>
