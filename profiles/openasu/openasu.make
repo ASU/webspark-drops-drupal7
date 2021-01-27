1f64c64c5e9cbed471020c3af59e03eaa5a23f4d
Bryan R <aubjr@asu.edu>
2021-01-27 12:42:37 -0700

    Webspark Release - Wacahoota (v1.76.4) - Please review the release notes on brandguide.asu.edu before applying!

--- profiles/openasu/openasu.make
+++ profiles/openasu/openasu.make
@@ -6,7 +6,7 @@ api = 2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 projects[drupal][type] = core
-projects[drupal][version] = 7.77
+projects[drupal][version] = 7.78
 
 ; Drupal Core Patches
 
--- profiles/openasu/themes/college/template.php
+++ profiles/openasu/themes/college/template.php
@@ -74,7 +74,7 @@ function college_preprocess_html(&$variables) {
       '#tag' => 'meta',
       '#attributes' => array(
         // Don't forget to update openasu.info as well!!
-        'content' => 'Webspark:1.76.3 (Jacksonville)',
+        'content' => 'Webspark:1.76.4 (Wacahoota)',
         'http-equiv' => 'X-Name-of-Distro',
         'name' => 'cmsversion',
       )
